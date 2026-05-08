80ddc0b13cdfb1218b6349d2758d7830a2494b82
nobbele <realnobbele@gmail.com>
2026-05-08 22:10:47 +0200

    Add mouse drawing scene

---
+++ Scripts/MouseDraw.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class MouseDraw : Node2D
+{
+    [Export] private Line2D line;
+    
+    public override void _Process(double delta)
+    {
+        var mousePosition = GetViewport().GetCamera2D()?.GetGlobalMousePosition() ??  GetViewport().GetMousePosition();
+        if (Input.IsActionJustPressed("left_click"))
+        {
+            AddEdge(mousePosition);
+        }
+
+        if (line.Points.Length > 0)
+        {
+            line.SetPointPosition(line.Points.Length - 1, mousePosition);
+        }
+    }
+
+    private void AddEdge(Vector2 pos)
+    {
+        if (line.Points.Length == 0)
+        {
+            line.AddPoint(pos);
+        }
+
+        line.AddPoint(pos);
+    }
+}

98ab863552e41f195e21ebbb155344dd0fb15f88
nobbele <realnobbele@gmail.com>
2026-05-09 10:47:45 +0200

    Require valid target for mouse draw

--- Scripts/MouseDraw.cs
+++ Scripts/MouseDraw.cs
@@ -4,6 +4,12 @@ using System;
 public partial class MouseDraw : Node2D
 {
     [Export] private Line2D line;
+
+    public Func<Vector2, bool> IsValidPlacement = _ => true;
+    public Func<Vector2, bool> IsTarget = _ => true;
+
+    [Signal]
+    delegate void OnCompleteEventHandler(Vector2I[] edges);
     
     public override void _Process(double delta)
     {
@@ -26,7 +32,22 @@ public partial class MouseDraw : Node2D
 
     private void AddEdge(Vector2 pos)
     {
-        if (line.Points.Length == 0)
+        if (!IsValidPlacement(pos))
+        {
+            return;
+        }
+
+        var isTarget = IsTarget(pos);
+        var isFirst = line.Points.Length == 0;
+
+        // Reject if the starting node wasn't a valid starting target.
+        if (isFirst && !isTarget)
+        {
+            return;
+        }
+        
+        // One node ahead so the last one can be previewed.
+        if (isFirst)
         {
             line.AddPoint(pos);
         }
@@ -42,5 +63,10 @@ public partial class MouseDraw : Node2D
         }
         
         line.RemovePoint(line.Points.Length - 1);
+    }
+
+    private void Complete()
+    {
+        
     }
 }
